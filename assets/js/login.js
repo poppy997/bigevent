fa8e71a8f9df56cd4c599b698531547ef285fa6d
poppy <poppy@qq.com>
2020-09-10 11:18:22 +0800

    master

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,8 +4,13 @@ $(function() {
         $('.reg-form').show()
     })
     $('#showLogin').on('click', function() {
-        $('.login-form').show()
-        $('.reg-form').hide()
+            $('.login-form').show()
+            $('.reg-form').hide()
+        })
+        //从layui中获取form对象
+    var form = layui.form
+        //通过form.varify()自定义校验规则
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位,且不能出现空格']
     })
-
 })
